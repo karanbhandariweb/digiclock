acb7423595f6597c01a3c6405aab538c5472efde
insert github real name here <insert github email here>
2020-10-14 10:53:03 +0530

    Updates

---
+++ precache-manifest.bef04c4aa9ace93933c9904db2c57865.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d76cf72898c8c9f53496e583a91562a5",
+    "url": "/digiclock/index.html"
+  },
+  {
+    "revision": "4dfdd3abf89095fb2e2d",
+    "url": "/digiclock/static/css/main.19c0c130.chunk.css"
+  },
+  {
+    "revision": "3b0fa0992a79527b0b98",
+    "url": "/digiclock/static/js/2.9d1805f4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/digiclock/static/js/2.9d1805f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4dfdd3abf89095fb2e2d",
+    "url": "/digiclock/static/js/main.f549caeb.chunk.js"
+  },
+  {
+    "revision": "19cc78ca28535d62a360",
+    "url": "/digiclock/static/js/runtime-main.b7dc7d26.js"
+  }
+]);
