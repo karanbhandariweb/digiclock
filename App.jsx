b88439295064b1b18914b929900635b04bbda03a
karanbhandariweb <72808089+karanbhandariweb@users.noreply.github.com>
2020-12-06 23:54:02 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Myclck from './Myclck';
+
+const App = ()=>{
+   
+    return(
+        <>
+        <a href="https://karanbhandariweb.github.io/portfolio" class="previous round">&#8249;</a>
+        <Myclck/>    
+        </>
+    );
+}
+export default App;
